add774651445e72372091e86d08f23c2450395f3
Ishpreet Kohli <30780342+JiaLak99@users.noreply.github.com>
2017-08-12 22:02:06 -0400

    Add files via upload

---
+++ pointer.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+void update(int *a,int *b) {
+    *a += *b;
+    *b = abs(*a - *b - *b);  
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
